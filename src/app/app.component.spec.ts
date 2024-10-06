be43ecc2b592970fb14a619eae32dbab4f6e3dc2
Milad Raeisi <miladsoft@yahoo.com>
2024-10-06 18:48:29 +0400

    Update code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-material-3' title`, () => {
+  it(`should have the 'exchange' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-material-3');
+    expect(app.title).toEqual('exchange');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-material-3');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, exchange');
   });
 });
